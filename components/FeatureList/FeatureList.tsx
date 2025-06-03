5bb9f35c9ac3d7f98fa75c5db39f9b76107a2246
JingxuanWei <weijingxuan925@163.com>
2025-06-03 18:46:46 -0300

    feature list 修改和样式表

--- components/FeatureList/FeatureList.tsx
+++ components/FeatureList/FeatureList.tsx
@@ -8,21 +8,21 @@ const FeatureList = () => {
                 cardTitle='Hygienic Food'
                 cardDescription='Impeccable food quality, guaranteed safety, and freshness at all times.'
             />
-            <FeatureItem
-                cardNumber={2}
-                cardTitle='Fresh Environment'
-                cardDescription='Inviting ambiance, fostering a fresh and vibrant dining experience.'
-            />
+            {/*<FeatureItem*/}
+            {/*    cardNumber={2}*/}
+            {/*    cardTitle='Fresh Environment'*/}
+            {/*    cardDescription='Inviting ambiance, fostering a fresh and vibrant dining experience.'*/}
+            {/*/>*/}
             <FeatureItem
                 cardNumber={3}
                 cardTitle='Skilled Chefs'
                 cardDescription='Masterful chefs crafting culinary delights with precision and expertise.'
             />
-            <FeatureItem
-                cardNumber={4}
-                cardTitle='Event & Party'
-                cardDescription='Perfect venue for unforgettable events and lively celebrations.'
-            />
+            {/*<FeatureItem*/}
+            {/*    cardNumber={4}*/}
+            {/*    cardTitle='Event & Party'*/}
+            {/*    cardDescription='Perfect venue for unforgettable events and lively celebrations.'*/}
+            {/*/>*/}
         </ul>
     )
 }
