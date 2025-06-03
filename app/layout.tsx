f17b6dac7019eea267dc767e78a8249b03141674
JingxuanWei <weijingxuan925@163.com>
2025-06-03 13:09:32 -0300

    保持主页可见footer，home页目前格式有错误

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,20 +4,22 @@ import './globals.css';
 import { BackToTop, Footer, Navbar, TopBar } from '@/components/components';
 
 export const metadata: Metadata = {
-  title: 'Poke go & broth house',
-  description: 'Discover the best dishes in the world',
+    title: 'Grilli Restaurant',
+    description: 'Discover the best dishes in the world',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-      <html lang="en">
-      <body style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', margin: 0 }}>
-      <TopBar />
-      <Navbar />
-      <main style={{ flex: 1 }}>{children}</main>
-      <Footer />
-      </body>
-      </html>
-  );
+export default function RootLayout({children} : {children: React.ReactNode}) {
+    return (
+        <html lang="en">
+        <body id='top' className={`${dmSans.className} nav-active`}>
+        <TopBar/>
+        <Navbar/>
+        {children}
+        <Footer/>
+        <BackToTop/>
+        </body>
+        </html>
+    )
 }
+
 
