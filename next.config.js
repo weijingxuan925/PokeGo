0bfc238fb3c22124a1244cfd00d07bc3f4d94693
JingxuanWei <weijingxuan925@163.com>
2025-06-03 00:35:30 -0300

    github page 部署配置

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true, // 如果使用静态导出，可能需要禁用图片优化
+    },
+};
 
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = nextConfig;
