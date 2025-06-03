6ed19c1fad1d6d997ad055f93d969b6c656c6aae
JingxuanWei <weijingxuan925@163.com>
2025-06-03 01:13:31 -0300

    添加home page 组件

---
+++ app/home/page.tsx
@@ -0,0 +1,12 @@
+import styles from './HomePage.module.css';
+
+const HomePage = () => {
+    return (
+        <div className={styles.container}>
+            <h1>Home Page</h1>
+            <p>Welcome to the Home page!</p>
+        </div>
+    );
+};
+
+export default HomePage;
